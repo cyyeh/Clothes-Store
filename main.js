139e98e8906467f991706f8986d7d17bf78723fd
ChihYu Yeh <cyyeh@data-sci.info>
2021-12-27 21:18:08 +0800

    finish quick view

--- main.js
+++ main.js
@@ -2,39 +2,89 @@
 //       You can access this variable here, and should use this variable here to build your webpages
 
 let initProducts = () => {
+    const $Header = document.getElementsByTagName('header')[0]
+    const $Nav = document.getElementsByTagName('nav')[0]
+    const $Main = document.getElementsByTagName('main')[0]
+    const $Footer = document.getElementsByTagName('footer')[0]
     const QuickViewButtonsClassName = 'quick-view-btn'
     const DetailPageButtonsClassName = 'detail-page-btn'
 
+    // Start of Used-More-Than-Once Helper Methods
+    function getProductCardImage(colorsImages, defaultImages) {
+        console.assert(defaultImages, "There should be default images found!")
+        console.assert(defaultImages.front, "There should be at least a default front image!")
+
+        if (!colorsImages) {
+            return defaultImages.front
+        }
+        if (!Object.keys(colorsImages).length) {
+            return defaultImages.front
+        } else {
+            return colorsImages[Object.keys(colorsImages)[0]].front
+        }
+    }
+
+    function disableBackgroundStyles() {
+        $Header.style.opacity = 0.5
+        $Nav.style.opacity = 0.5
+        $Main.style.opacity = 0.5
+        $Footer.style.opacity = 0.5
+        $Header.style.pointerEvents = 'none'
+        $Nav.style.pointerEvents = 'none'
+        $Main.style.pointerEvents = 'none'
+        $Footer.style.pointerEvents = 'none'
+    }
+
+    function enableBackgroundStyles() {
+        $Header.style.opacity = 1
+        $Nav.style.opacity = 1
+        $Main.style.opacity = 1
+        $Footer.style.opacity = 1
+        $Header.style.pointerEvents = 'inherit'
+        $Nav.style.pointerEvents = 'inherit'
+        $Main.style.pointerEvents = 'inherit'
+        $Footer.style.pointerEvents = 'inherit'
+    }
+
+    function registerOnClickEvents(elements, event) {
+        for (let clement of elements) {
+            clement.addEventListener('click', event)
+        }
+    }
+
+    function removeOnClickEvents(elements, event) {
+        for (let element of elements) {
+            element.removeEventListener('click', event)
+        }
+    }
+
+    function removeQuickViewWindow() {
+        const $QuickViewWindow = document.getElementById('quick-view-window')
+        if ($QuickViewWindow) {
+            $QuickViewWindow.remove()
+        }
+    }
+    // End of Used-More-Than-Once Helper Methods
+
     function renderProducts(products) {
+        function getProductAvailableColorsText(colors) {
+            if (!colors || !Object.keys(colors).length) {
+                return 'Available in 0 color'
+            } else if (Object.keys(colors).length === 1) {
+                return 'Available in 1 color'
+            } else {
+                return `Available in ${Object.keys(colors).length} colors`
+            }
+        }
+
         function getProductCardHTML(product, idx) {
-            function getProductCardImage(colorsImages, defaultImages) {
-                console.assert(defaultImages, "There should be default images found!")
-                console.assert(defaultImages.front, "There should be at least a default front image!")
-        
-                if (!colorsImages) {
-                    return defaultImages.front
-                }
-                if (!Object.keys(colorsImages).length) {
-                    return defaultImages.front
-                } else {
-                    return colorsImages[Object.keys(colorsImages)[0]].front
-                }
-            }
-    
-            function getProductAvailableColorsText(colors) {
-                if (!colors || !Object.keys(colors).length) {
-                    return 'Available in 0 color'
-                } else if (Object.keys(colors).length === 1) {
-                    return 'Available in 1 color'
-                } else {
-                    return `Available in ${Object.keys(colors).length} colors`
-                }
-            }
-    
             return `
                 <article>
                     <section class="product-image-container">
-                        <img src=${getProductCardImage(product.colors, product.default)} alt=${product.name} />
+                        <img
+                            src=${getProductCardImage(product.colors, product.default)}
+                            alt=${product.name}
+                        />
                     </section>
                     <section class="product-info-container">
                         <h3>${product.name}</h3>
@@ -67,24 +117,60 @@ let initProducts = () => {
                 getProductCardHTML(shirt, idx)
             )
         }
-    }
 
-    function registerOnClickEvent(className, event) {
-        const elements = document.getElementsByClassName(className)
-        for (let button of elements) {
-            button.addEventListener('click', event)
-        }
-    }
-
-    function removeOnClickEvent(className, event) {
-        const elements = document.getElementsByClassName(className)
-        for (let button of elements) {
-            button.removeEventListener('click', event)
-        }
+        // register onClick event listeners
+        registerOnClickEvents(
+            document.getElementsByClassName(QuickViewButtonsClassName),
+            clickProductQuickView
+        )
+        registerOnClickEvents(
+            document.getElementsByClassName(DetailPageButtonsClassName),
+            clickProductDetailPage
+        )
     }
 
     function clickProductQuickView(event) {
-        console.log(event.target.dataset.index)
+        function renderProductQuickView(product) {
+            function getProductQuickViewHTML(product) {
+                return `
+                    <article id="quick-view-window">
+                        <button
+                            type="button"
+                            id="quick-view-window-close-btn"
+                        >
+                            &times;
+                        </button>
+                        <section class="product-image-container">
+                            <img
+                                src=${getProductCardImage(product.colors, product.default)}
+                                alt=${product.name}
+                            />
+                        </section>
+                        <section class="product-info-container">
+                            <h3>${product.name}</h3>
+                            <p class="price">${product.price}</p>
+                            <p>${product.description}</p>
+                        </section>
+                    </article>
+                `
+            }
+
+            // remove quick view window if it's now being rendered, just in case
+            removeQuickViewWindow()
+            disableBackgroundStyles()
+            document.body.insertAdjacentHTML(
+                'beforeend',
+                getProductQuickViewHTML(product)
+            )
+
+            // register onClick event listeners
+            registerOnClickEvents(
+                [document.getElementById('quick-view-window-close-btn')],
+                clickCloseProductQuickViewWindow
+            )
+        }
+
+        renderProductQuickView(window.shirts[event.target.dataset.index])
     }
 
     function clickProductDetailPage(event) {
@@ -93,23 +179,37 @@ let initProducts = () => {
         window.location = `${urlElements.join('/')}?id=${event.target.dataset.index}`
     }
 
-    renderProducts(window.shirts)
+    function clickCloseProductQuickViewWindow() {
+        enableBackgroundStyles()
+        removeQuickViewWindow()
+    }
 
-    // register onClick event listeners
-    registerOnClickEvent(QuickViewButtonsClassName, clickProductQuickView)
-    registerOnClickEvent(DetailPageButtonsClassName, clickProductDetailPage)
+    // render products objects to document and register onClick events
+    renderProducts(window.shirts)
 
     window.onunload = () => {
         console.log('unloading...')
         // remove all onClick event listeners before changing page link
-        removeOnClickEvent(QuickViewButtonsClassName, clickProductQuickView)
-        removeOnClickEvent(DetailPageButtonsClassName, clickProductDetailPage)
+        removeOnClickEvents(
+            document.getElementsByClassName(QuickViewButtonsClassName),
+            clickProductQuickView
+        )
+        removeOnClickEvents(
+            document.getElementsByClassName(DetailPageButtonsClassName),
+            clickProductDetailPage
+        )
+        const $QuickViewWindowCloseButton = document.getElementById('quick-view-window-close-btn')
+        if ($QuickViewWindowCloseButton) {
+            removeOnClickEvents(
+                [$QuickViewWindowCloseButton],
+                clickCloseProductQuickViewWindow
+            )
+        }
     }
 };
 
 let initDetails = () => {
-    // To see the shirts object, run:
-    // console.log(shirts);
-
     // Your Code Here
+    console.log(window.shirts)
+    console.log(window.location.href)
 };
