f38031bec9116755a8c53966d7e2574b8e17bf5b
ChihYu Yeh <cyyeh@data-sci.info>
2021-12-28 00:33:27 +0800

    add details page, WIP for button styles

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-// NOTE: The variable "shirts" is defined in the shirts.js file as the full list of shirt offerings
-//       You can access this variable here, and should use this variable here to build your webpages
+// general methods for both pages
+function getProductCardImage(colorsImages, defaultImages, side='front', color='default') {
+    console.assert(defaultImages, "There should be default images found!")
+    console.assert(defaultImages.front, "There should be at least a default front image!")
+
+    if (!colorsImages) {
+        return defaultImages[side]
+    }
+    if (!Object.keys(colorsImages).length) {
+        return defaultImages[side]
+    } else {
+        if (color === 'default') {
+            return colorsImages[Object.keys(colorsImages)[0]][side]
+        } else {
+            return colorsImages[color][side]
+        }
+        
+    }
+}
+
+function registerOnClickEvents(elements, event) {
+    for (let clement of elements) {
+        clement.addEventListener('click', event)
+    }
+}
+
+function removeOnClickEvents(elements, event) {
+    for (let element of elements) {
+        element.removeEventListener('click', event)
+    }
+}
 
 let initProducts = () => {
     const $Header = document.getElementsByTagName('header')[0]
@@ -10,20 +39,6 @@ let initProducts = () => {
     const DetailPageButtonsClassName = 'detail-page-btn'
 
     // Start of Used-More-Than-Once Helper Methods
-    function getProductCardImage(colorsImages, defaultImages, direction='front') {
-        console.assert(defaultImages, "There should be default images found!")
-        console.assert(defaultImages.front, "There should be at least a default front image!")
-
-        if (!colorsImages) {
-            return defaultImages[direction]
-        }
-        if (!Object.keys(colorsImages).length) {
-            return defaultImages[direction]
-        } else {
-            return colorsImages[Object.keys(colorsImages)[0]][direction]
-        }
-    }
-
     function disableBackgroundStyles() {
         $Header.style.opacity = 0.5
         $Nav.style.opacity = 0.5
@@ -46,17 +61,6 @@ let initProducts = () => {
         $Footer.style.pointerEvents = 'inherit'
     }
 
-    function registerOnClickEvents(elements, event) {
-        for (let clement of elements) {
-            clement.addEventListener('click', event)
-        }
-    }
-
-    function removeOnClickEvents(elements, event) {
-        for (let element of elements) {
-            element.removeEventListener('click', event)
-        }
-    }
 
     function removeQuickViewWindow() {
         const $QuickViewWindow = document.getElementById('quick-view-window')
@@ -139,7 +143,7 @@ let initProducts = () => {
                             alt=${productName}
                         />
                         <img
-                            src=${getProductCardImage(colorsImages, defaultImages, direction='back')}
+                            src=${getProductCardImage(colorsImages, defaultImages, side='back')}
                             alt=${productName}
                         />
                     </div>
@@ -221,7 +225,141 @@ let initProducts = () => {
 };
 
 let initDetails = () => {
-    // Your Code Here
-    console.log(window.shirts)
-    console.log(window.location.href)
+    const id = parseInt(
+        new URLSearchParams(window.location.search).get('id')
+    )
+    const shirt = window.shirts[id]
+    const detailState = {
+        'side': 'front',
+        'color': (
+            Object.keys(shirt.colors).length
+            ?
+            Object.keys(shirt.colors)[0]
+            :
+            ''
+        )
+    }
+    const ProductTypeButtonClassName = 'product-type-button'
+
+    function renderProduct(product) {
+        function renderColorButtons(colors, defaultColor) {
+            function capitalizeFirstLetter(letters) {
+                return letters.charAt(0).toUpperCase() + letters.slice(1)
+            }
+
+            function getColorButtonsHTML(colors, defaultColor) {
+                let colorButtonsHTML = ''
+
+                for (let color of colors) {
+                    colorButtonsHTML += `
+                        <input
+                            id="color-${color}"
+                            type="radio"
+                            name="color"
+                            vlaue=${color}
+                            ${defaultColor === color ? "checked" : ""}
+                            class=${ProductTypeButtonClassName}
+                        />
+                        <label for="color-${color}">${capitalizeFirstLetter(color)}</label>
+                    `
+                }
+
+                return colorButtonsHTML
+            }
+
+            return `
+                <section class="form-section">
+                    <span>Color:</span>
+                    ${getColorButtonsHTML(colors, defaultColor)}
+                </section>
+            `
+        }
+
+        function getProductDetailHTML(product) {
+            return `
+                <h2>${product.name}</h2>
+                <div id="details-container">
+                    <section class="details-image-container">
+                        <img
+                            src=${getProductCardImage(
+                                product.colors,
+                                product.default,
+                                side=detailState.side,
+                                color=detailState.color,
+                            )}
+                            alt=${product.name}
+                            id="details-image"
+                        />
+                    </section>
+                    <section class="details-info-container">
+                        <p class="price">${product.price}</p>
+                        <p class="description">${product.description}</p>
+                        <form>
+                            <section class="form-section">
+                                <span>Side:</span>
+                                <input
+                                    id="side-front"
+                                    type="radio"
+                                    name="side"
+                                    vlaue="front"
+                                    class=${ProductTypeButtonClassName}
+                                    checked
+                                />
+                                <label for="side-front">Front</label>
+                                <input
+                                    id="side-back"
+                                    type="radio"
+                                    name="side"
+                                    value="back"
+                                    class=${ProductTypeButtonClassName}
+                                />
+                                <label for="side-back">Back</label>
+                            </section>
+                            ${
+                                detailState.color
+                                ?
+                                renderColorButtons(Object.keys(product.colors), detailState.color)
+                                :
+                                ''
+                            }
+                        </form>
+                    </section>
+                </div>
+            `
+        }
+
+        const $DetailsContainer = document.getElementById('details')
+        $DetailsContainer.insertAdjacentHTML(
+            'beforeend',
+            getProductDetailHTML(product)
+        )
+
+        // register onClick event listeners
+        registerOnClickEvents(
+            document.getElementsByClassName(ProductTypeButtonClassName),
+            clickProductTypeButton
+        )
+    }
+
+    function clickProductTypeButton(event) {
+        const [productType, typeValue] = event.target.id.split('-')
+        detailState[productType] = typeValue
+        const $DetailsImage = document.getElementById('details-image')
+        $DetailsImage.src = getProductCardImage(
+            shirt.colors,
+            shirt.default,
+            side=detailState.side,
+            color=detailState.color,
+        )
+    }
+
+    renderProduct(shirt)
+
+    window.unload = () => {
+        console.log('unloading...')
+        removeOnClickEvents(
+            document.getElementsByClassName(ProductTypeButtonClassName),
+            clickProductTypeButton
+        )
+    }
 };
